deb730eb8db700d86415272f97cf06e2c392399b
Leonid Ivankin <lim14@yandex.ru>
2018-05-03 21:17:22 +0300

    fix merge

--- app/src/main/java/ru/leonidivankin/photovkapp/MainActivity.java
+++ app/src/main/java/ru/leonidivankin/photovkapp/MainActivity.java
@@ -2,19 +2,17 @@ package ru.leonidivankin.photovkapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +32,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 		ButterKnife.bind(this);
 		addFabListener();
+
+		setSupportActionBar(toolbar);
+
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
+
+		navigationView.setNavigationItemSelectedListener(this);
 	}
 
 	private void addFabListener() {
@@ -47,15 +54,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	public void onClickSnackbar(View view) {
 		Snackbar.make(view, "Добавлено фото", Snackbar.LENGTH_LONG).show();
-
-		setSupportActionBar(toolbar);
-
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-
-		navigationView.setNavigationItemSelectedListener(this);
 	}
 
 	@Override
